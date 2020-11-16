96823248c9de7419730d40b09c280ea546df2490
OJK <57939880+Oh123456@users.noreply.github.com>
2020-11-16 14:46:07 +0900

    캐릭터함수 생성

--- shikhondo/Character.h
+++ shikhondo/Character.h
@@ -1,5 +1,21 @@
 #pragma once
-class Character
+#include "pch.h"
+#include "GameNode.h"
+class Character : public GameNode
 {
+public:
+
+	HRESULT Init();				// 멤버 변수 초기화, 메모리 할당
+	void Release();				// 메모리 해제
+	void Update();				// 프레임 단위 게임 로직 실행 (데이터 변경)
+	void Render(HDC hdc);		// 프레임 단위 출력 (이미지, 텍스트 등)
+
+protected:
+	int hp;
+	int damge;
+	FPOINT pos;
+	float speed;
+	SIZE size;
+
 };
 
