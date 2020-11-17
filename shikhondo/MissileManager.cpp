3030a9df5f5a3dec17963d2896607352aa1cd4b2
OJK <57939880+Oh123456@users.noreply.github.com>
2020-11-17 12:17:36 +0900

    EnemyManager,MissileManager 수정

--- shikhondo/MissileManager.cpp
+++ shikhondo/MissileManager.cpp
@@ -1,7 +1,12 @@
 #include "MissileManager.h"
+#include "Missile.h"
+#include "MainGame.h"
+#include "GamePlayStatic.h"
+#include "Character.h"
 
 HRESULT MissileManager::Init()
 {
+	SetIsValid(false);
 	return E_NOTIMPL;
 }
 
@@ -15,4 +20,30 @@ void MissileManager::Update()
 
 void MissileManager::Render(HDC hdc)
 {
+}
+
+Missile * MissileManager::CreateMissile(FPOINT pos)
+{
+	Missile* spwanMissile = GamePlayStatic::GetMainGame()->CreateObject<Missile>();
+	spwanMissile->SetPos(pos);
+	spwanMissile->SetIsValid(false);
+	return spwanMissile;
+}
+
+Missile* MissileManager::SpwanMissile(Character* onwer, FPOINT pos)
+{
+	list<Missile*>::iterator mit;
+	Missile* missile;
+	for (mit = missileList.begin(); mit != missileList.end(); mit++)
+	{
+		missile = *mit;
+		if (!missile->GetIsValid())
+		{
+			missile->SetIsValid(true);
+			missile->SetOwner(onwer);
+			missile->SetPos(pos);
+			missileList.splice(missileList.end(), missileList, mit);
+			break;
+		}
+	}
 }
--- shikhondo/MissileManager.h
+++ shikhondo/MissileManager.h
@@ -1,16 +1,24 @@
 #pragma once
 #include "pch.h"
-#include "Singleton.h"
+#include "GameNode.h"
 
-class MissileManager : public Singleton<MissileManager>
+
+class MissileManager : public GameNode
 {
-private:
-
 
 public:
 	HRESULT Init();
 	void Release();
 	void Update();				// 프레임 단위 게임 로직 실행 (데이터 변경)
 	void Render(HDC hdc);		// 프레임 단위 출력 (이미지, 텍스트 등
+
+	//미사일 생성
+	class Missile* CreateMissile(FPOINT pos = { -100.0f,-100.0f });
+	// 생성된 미사일을 가져다 쓰는거
+	class Missile* SpwanMissile(class Character* onwer,FPOINT pos = { 0.0f,0.0f });
+
+
+private:
+	list<Missile*> missileList;
 };
 
