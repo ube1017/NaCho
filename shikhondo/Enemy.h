f8afd162bf9605be8a8efa32052b5ebd88c4d4f1
nachos1424 <73320992+nachos1424@users.noreply.github.com>
2020-11-17 12:26:50 +0900

    Update Enemy.h

--- shikhondo/Enemy.h
+++ shikhondo/Enemy.h
@@ -9,6 +9,10 @@ public:
 	void Release();				// 메모리 해제
 	void Update();				// 프레임 단위 게임 로직 실행 (데이터 변경)
 	void Render(HDC hdc);		// 프레임 단위 출력 (이미지, 텍스트 등
+
+	virtual void RandLocation() {}
+	virtual void LocationReset() {}
+
 protected:
 	FPOINT RandPos;
 	int RandNum;
