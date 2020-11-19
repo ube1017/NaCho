3db5d53ad9d5f5d943b150f03496733c705f3d91
ube1017 <72431135+ube1017@users.noreply.github.com>
2020-11-19 15:21:08 +0900

    asd

--- shikhondo/PlayScene.cpp
+++ shikhondo/PlayScene.cpp
@@ -38,7 +38,7 @@ HRESULT PlayScene::Init()
 	player->SetMissileManager(missileManager);
 	enemyManager->SetMainGame(this);
 	for (int i = 0; i < 10; i++)
-		enemyManager->CreateEeney<Enemy2>(missileManager);
+		enemyManager->CreateEeney<Enemy3>(missileManager);
 	for (int i = 0; i < MAX_MISSILE; i++)
 		missileManager->CreateMissile();
 	/*FPOINT ppos;*/
